4e5d79c302ff448aaec48320c48d835a874ccaa8
Jan Petržílka <zilis14ys@icloud.com>
2026-02-17 22:30:22 +0100

    První commit - základní struktura webu

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+  echo "<h1>Vítejte v mém obchodě s tropickým ovocem!</h1>";
+  echo "<p>Pokud vidíte tento text, PHP funguje správně.</p>";
+  phpinfo(); // Toto zobrazí detaily o tvém serveru
+?>
